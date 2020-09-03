ed713cb103b8b0bedd7277f58807d1c773f52899
Joshuaschist <53118782+Joshuaschist@users.noreply.github.com>
2020-09-03 06:32:02 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Containers/App';
-import * as serviceWorker from './serviceWorker';
-import 'tachyons';
- 
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.register();
